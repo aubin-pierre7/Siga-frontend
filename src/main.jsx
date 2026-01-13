5e1e4ff14ba22ef1f46dc21ca7af01436e144ebb
aubin-pierre7 <aubinp58@gmail.com>
2026-01-14 00:21:10 +0100

    Modification du fichier principale

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import App from './App'
+import { AuthProvider } from './context/AuthContext'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+        <ToastContainer 
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
